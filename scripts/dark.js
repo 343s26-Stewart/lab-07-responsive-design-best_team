00c9d3d3e0a446273eabd031ae64a76267d85085
mackenzierossiter <rossitermackenzie@gmail.com>
2026-03-11 14:56:49 -0400

    Added the dark/light mode feature

---
+++ scripts/dark.js
@@ -0,0 +1,40 @@
+function main() {
+    let navItems = document.querySelectorAll('.main-nav li');
+    let OSDefault = navItems[0];
+    let darkMode = navItems[1];
+    let lightMode = navItems[2];
+    let body = document.querySelector('body');
+
+    // const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    // if (prefersDarkMode) {
+    //     localStorage.setItem("Dark");
+    // } else {
+    //     localStorage.setItem("Light");
+    // }
+
+    OSDefault.addEventListener('click', ()=>{
+        const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if (prefersDarkMode) {
+            body.classList.remove("lightMode");
+            body.classList.add("darkMode");
+        } else {
+            body.classList.remove("darkMode");
+            body.classList.add("lightMode");
+        }
+        event.preventDefault() 
+    });
+
+    darkMode.addEventListener('click', ()=>{
+        body.classList.remove("lightMode");
+        body.classList.add("darkMode");
+        console.log(body.classList);
+        event.preventDefault() 
+    })
+
+    lightMode.addEventListener('click', ()=>{
+        body.classList.remove("darkMode");
+        body.classList.add("lightMode");
+        event.preventDefault() 
+    })
+}
+document.addEventListener('DOMContentLoaded', main)
